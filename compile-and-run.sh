2ff06fd8ae340a5f2f7ad7acb6c4ab7fc91c52b8
Tim <tim.breiter98@gmail.com>
2026-07-06 00:28:31 +0200

    verbessert

--- compile-and-run.sh
+++ compile-and-run.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 rm -rf out
 mkdir -p out
-javac --release 17 -encoding UTF-8 -d out $(find src/main/java -name '*.java')
+javac --release 25 -encoding UTF-8 -d out $(find src/main/java -name '*.java')
 java -Dfile.encoding=UTF-8 -cp out zoo.Demo
